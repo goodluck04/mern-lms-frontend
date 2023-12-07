b0298099b41d738c29932f9de1809b28217e1ce3
goodluck04 <goodluckkumar04@gmail.com>
2023-12-07 14:14:38 +0530

    completer navbar with mobile responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
 "use client"
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
+import Header from "./components/Header";
 
 interface Props { }
 
 const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
   return (
     <div>
+      {/* metadata */}
       <Heading
         title="LMS Home"
         description="E learning platform for student to learn and get help from teachers"
         keywords="Programmin,Redux,ML,"
       />
+      {/* Header */}
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+      />
+      
     </div>
   )
 }
