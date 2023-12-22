0f5cb6cc6d361135c24700fbf2f7fdb8a8945f58
goodluck <goodluckkumar04@gmail.com>
2023-12-22 16:08:34 +0530

    axios

--- redux/features/courses/coursesApi.ts
+++ redux/features/courses/coursesApi.ts
@@ -3,7 +3,7 @@ import { apiSlice } from "../api/apiSlice";
 export const courseApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     createCourse: builder.mutation({
-      query: (data) => ({
+      query: (data) => ({ 
         url: "create-course",
         method: "POST",
         body: data,
