293b173bd3ce9aedec04e0f7ecef3006e6190816
goodluck <goodluckkumar04@gmail.com>
2023-12-31 18:59:00 +0530

    footer done

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import Header from "./components/Header";
 import Hero from "./components/Route/Hero";
 import Courses from "./components/Route/Courses";
 import Reviews from "./components/Route/Reviews";
-import FAQ from "./components/FAQ/FAQ"
+import FAQ from "./components/FAQ/FAQ";
+import Footer from "./components/Footer/Footer"
 
 
 interface Props { }
